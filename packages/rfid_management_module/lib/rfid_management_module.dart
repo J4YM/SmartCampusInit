90b7ba6e7735e5cd0148500b4361aa6ca69874ba
J4YM <rubenbanaga@gmail.com>
2026-08-23 16:16:08 +0800

    Add Technical Issues tab with ticket queue and comment thread

--- packages/rfid_management_module/lib/rfid_management_module.dart
+++ packages/rfid_management_module/lib/rfid_management_module.dart
@@ -5,3 +5,4 @@ export 'ui/it_technician_dashboard_page.dart';
 export 'ui/rfid_dashboard_page.dart';
 export 'ui/rfid_reader_management_page.dart';
 export 'ui/student_records_tab.dart';
+export 'ui/technical_issues_tab.dart';
